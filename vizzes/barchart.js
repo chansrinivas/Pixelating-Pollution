4e18476d6b9141f4f173ef2ffbc40745757c59cc
Chandana <csrinivas2@dons.usfca.edu>
2024-04-12 19:41:01 -0700

    add vizzes for alpha release

---
+++ vizzes/barchart.js
@@ -0,0 +1,111 @@
+// <!-- <!DOCTYPE html>
+// <meta charset="utf-8">
+
+
+// <script src="https://d3js.org/d3.v6.js"></script>
+
+// <div id="my_dataviz"></div>
+
+// <script> -->
+// var container = d3.select('#d3-viz-container');
+
+// set the dimensions and margins of the graph
+const margin = { top: 10, right: 30, bottom: 70, left: 50 },
+    width = 860 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#d3-viz-container")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Parse the Data
+d3.csv("https://gist.githubusercontent.com/chansrinivas/3a8ebf18a807d920891110528f58b463/raw/346321afbf500adde192b9bac96fcc32ed952c00/barchart-bayarea.csv").then(function (data) {
+
+    // List of subgroups = header of the csv files = soil condition here
+    const subgroups = data.columns.slice(1);
+
+    // List of groups = species here = value of the first column called group -> I show them on the X axis
+    const groups = data.map(d => d.date);
+
+    // Add X axis
+    const x = d3.scaleBand()
+        .domain(groups)
+        .range([0, width])
+        .padding([0.2])
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x).tickSize(0));
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+        .domain([0, 150])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add y axis label
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Daily AQI for PM 2.5 Value");
+
+    // Calculate the width of each group
+    const barWidth = x.bandwidth();
+
+    // Another scale for subgroup position
+    const xSubgroup = d3.scaleBand()
+        .domain(subgroups)
+        .range([0, barWidth])
+        .padding([0.05]);
+
+    // color palette = one color per subgroup
+    const color = d3.scaleOrdinal()
+        .domain(subgroups)
+        .range(['#7fc97f', '#beaed4', '#fdc086', '#ffff99']);
+
+    // Show the bars
+    svg.append("g")
+        .selectAll("g")
+        .data(data)
+        .join("g")
+        .attr("transform", d => `translate(${x(d.date)}, 0)`) // Adjust x-position based on date
+        .selectAll("rect")
+        .data(function (d) { return subgroups.map(function (key) { return { key: key, value: d[key] }; }); })
+        .join("rect")
+        .attr("x", d => xSubgroup(d.key)) // Adjust x-position based on subgroup
+        .attr("y", d => y(d.value))
+        .attr("width", xSubgroup.bandwidth())
+        .attr("height", d => height - y(d.value))
+        .attr("fill", d => color(d.key));
+
+    // Add legend
+    const legendLabels = ["San Francisco", "San Jose", "Oakland", "Fremont"];
+
+    const legend = svg.append("g")
+        .attr("transform", `translate(${width - 100},${margin.top})`); // Adjust position for vertical legend
+
+    legendLabels.forEach((label, i) => {
+        const legendRow = legend.append("g")
+            .attr("transform", `translate(0, ${i * 20})`); // Adjust position for vertical legend
+
+        legendRow.append("rect")
+            .attr("width", 10)
+            .attr("height", 10)
+            .attr("fill", color(subgroups[i]));
+
+        legendRow.append("text")
+            .attr("x", 15)
+            .attr("y", 10)
+            .attr("dy", ".35em")
+            .text(label);
+    });
+})
+
+// <!-- </script> -->
--- vizzes/heatmap.js
+++ vizzes/heatmap.js
@@ -1,72 +1,72 @@
-<!doctype html>
-<html lang="en">
+// <!doctype html>
+// <html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>D3 Heat Map</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-        }
+// <head>
+//     <meta charset="UTF-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>D3 Heat Map</title>
+//     <style>
+//         body {
+//             font-family: 'Arial', sans-serif;
+//         }
 
-        #chart-container {
-            position: relative;
-        }
+//         #chart-container {
+//             position: relative;
+//         }
 
-        .chart-background {
-            fill: #f0f0f0;
-        }
+//         .chart-background {
+//             fill: #f0f0f0;
+//         }
 
-        .heatmap-rect {
-            stroke: #fff;
-            stroke-width: 1.2px;
+//         .heatmap-rect {
+//             stroke: #fff;
+//             stroke-width: 1.2px;
             
-        }
+//         }
 
-        .chart-label {
-            font-size: 18px;
-            font-weight: bold;
-            fill: #140431;
-        }
-    </style>
+//         .chart-label {
+//             font-size: 18px;
+//             font-weight: bold;
+//             fill: #140431;
+//         }
+//     </style>
 
-    <script src="https://d3js.org/d3.v7.min.js"></script>
-    <script src="https://d3js.org/d3-scale-chromatic.v2.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3-legend/2.25.6/d3-legend.min.js"></script>
+//     <script src="https://d3js.org/d3.v7.min.js"></script>
+//     <script src="https://d3js.org/d3-scale-chromatic.v2.min.js"></script>
+//     <script src="https://cdnjs.cloudflare.com/ajax/libs/d3-legend/2.25.6/d3-legend.min.js"></script>
 
-</head>
+// </head>
 
-<body>
-    <div id="chart-container"></div>
-    <script>
+// <body>
+//     <div id="chart-container"></div>
+//     <script>
 
         var canvasWidth = 1400;
-        var canvasHeight = 400;
-        var margin = {
+        var canvasHeight = 500;
+        var margin1 = {
             top: 50,
             right: 600,
-            bottom: 50,
+            bottom: 150,
             left: 150,
         };
 
-        var svg = d3
+        var svg1 = d3
             .select('#chart-container')
             .append('svg')
             .attr('width', canvasWidth)
             .attr('height', canvasHeight);
 
 
-        var width = canvasWidth - margin.left - margin.right;
-        var height = canvasHeight - margin.top - margin.bottom;
+        var width1 = canvasWidth - margin1.left - margin1.right;
+        var height1 = canvasHeight - margin1.top - margin1.bottom;
 
         var yScale = d3
             .scaleBand()
-            .range([0, height])
+            .range([0, height1])
             .padding(0.03);
         var xScale = d3
             .scaleBand()
-            .range([0, width])
+            .range([0, width1])
             .padding(0.05);
 
         var heatmapColor = d3
@@ -75,9 +75,9 @@
             .domain([140, 20]); // Reversed domain to have green for lower values and red for higher values
 
 
-        var container_g = svg
+        var container_g = svg1
             .append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')',);
+            .attr('transform', 'translate(' + margin1.left + ',' + margin1.top + ')',);
 
         d3.csv(
             'https://gist.githubusercontent.com/chansrinivas/fd27e6043da02a8bde32d72a8440dd32/raw/00f25153eb2f1c10f1b653993c54cfb086605c4e/ozone.csv',
@@ -95,21 +95,21 @@
             );
 
             // Add chart title
-            svg
+            svg1
                 .append('text')
                 .attr('class', 'chart-label')
                 .attr('x', canvasWidth / 4.6)
-                .attr('y', margin.top / 2)
+                .attr('y', margin1.top / 2)
                 .text('Ground Level Ozone in Bay Area in 2023');
 
             // Add x-axis and label
             container_g
                 .append('g')
-                .attr('transform', 'translate(0, ' + height + ')')
+                .attr('transform', 'translate(0, ' + height1 + ')')
                 .call(d3.axisBottom(xScale))
                 .append('text')
                 .attr('y', 45)
-                .attr('x', width / 2)
+                .attr('x', width1 / 2)
                 .attr('font-family', 'sans-serif')
                 .attr('font-size', '14px')
                 .attr('fill', '#140431')
@@ -122,7 +122,7 @@
                 .append('text')
                 .attr('transform', 'rotate(-90)')
                 .attr('y', -120)
-                .attr('x', -height / 2.4)
+                .attr('x', -height1 / 2.4)
                 .attr('dy', '1em')
                 .attr('font-family', 'sans-serif')
                 .attr('font-size', '14px')
@@ -130,9 +130,9 @@
                 .text('Location');
 
             // Add legend
-            svg.append("g")
+            svg1.append("g")
                 .attr("class", "legendSequential")
-                .attr("transform", "translate(900,40)");
+                .attr("transform", "translate(130,440)");
 
             var legendSequential = d3.legendColor()
                 .title("Ground level Ozone")
@@ -141,7 +141,7 @@
                 .orient("horizontal")
                 .scale(heatmapColor);
 
-            svg.select(".legendSequential")
+                svg1.select(".legendSequential")
                 .call(legendSequential)
                 .selectAll("text")
                 .attr("font-size", "13px");
@@ -193,7 +193,7 @@
                 // .on("mouseleave", mouseleave)
 
         });
-    </script>
-</body>
+//     </script>
+// </body>
 
-</html>
+// </html>
