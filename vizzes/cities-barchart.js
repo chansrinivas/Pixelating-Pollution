6c5fa65587b831d3beeb7e774a61afc3863fac50
Chandana <csrinivas2@dons.usfca.edu>
2024-05-06 09:53:10 -0700

    fix bar chart interactions

--- vizzes/cities-barchart.js
+++ vizzes/cities-barchart.js
@@ -1,122 +1,123 @@
 const margin = { top: 10, right: 30, bottom: 70, left: 50 },
-            width = 860 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+    width = 860 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-            const tooltip = d3.select("#d3-viz-container")
-        .append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+const tooltip = d3.select("#d3-viz-container")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-        const svg = d3.select("#d3-viz-container")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
+const svg = d3.select("#d3-viz-container")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-        d3.csv("https://gist.githubusercontent.com/chansrinivas/3a8ebf18a807d920891110528f58b463/raw/eb7c3b89d6fb311ad6e127e899494ee783c0fa1d/barchart-bayarea.csv").then(function (data) {
+d3.csv("https://gist.githubusercontent.com/chansrinivas/3a8ebf18a807d920891110528f58b463/raw/eb7c3b89d6fb311ad6e127e899494ee783c0fa1d/barchart-bayarea.csv").then(function (data) {
 
-            const subgroups = data.columns.slice(1);
-            const groups = data.map(d => d.date);
+    const subgroups = data.columns.slice(1);
+    const groups = data.map(d => d.date);
 
-            const x = d3.scaleBand()
-                .domain(groups)
-                .range([0, width])
-                .padding([0.2]);
-            svg.append("g")
-                .attr("transform", `translate(0, ${height})`)
-                .call(d3.axisBottom(x).tickSize(0));
+    const x = d3.scaleBand()
+        .domain(groups)
+        .range([0, width])
+        .padding([0.2]);
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x).tickSize(0));
 
-            const y = d3.scaleLinear()
-                .domain([0, 150])
-                .range([height, 0]);
-            svg.append("g")
-                .call(d3.axisLeft(y));
+    const y = d3.scaleLinear()
+        .domain([0, 150])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
-            svg.append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 0 - margin.left)
-                .attr("x", 0 - (height / 2))
-                .attr("dy", "1em")
-                .style("text-anchor", "middle")
-                .text("Daily AQI for PM 2.5 Value");
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Daily AQI for PM 2.5 Value");
 
-            const barWidth = x.bandwidth();
+    const barWidth = x.bandwidth();
 
-            const xSubgroup = d3.scaleBand()
-                .domain(subgroups)
-                .range([0, barWidth])
-                .padding([0.05]);
+    const xSubgroup = d3.scaleBand()
+        .domain(subgroups)
+        .range([0, barWidth])
+        .padding([0.05]);
 
-            const color = d3.scaleOrdinal()
-                .domain(subgroups)
-                .range(['#EFEF6D', '#ff8072', '#bebada', '#8dd3c7']);
+    const color = d3.scaleOrdinal()
+        .domain(subgroups)
+        .range(['#EFEF6D', '#ff8072', '#bebada', '#8dd3c7']);
 
-            svg.append("g")
-                .selectAll("g")
-                .data(data)
-                .join("g")
-                .attr("transform", d => `translate(${x(d.date)}, 0)`)
-                .selectAll("rect")
-                .data(function (d) { return subgroups.map(function (key) { return { key: key, value: d[key] }; }); })
-                .join("rect")
-                .attr("class", d => d.key) // Assign class based on subgroup (city name)
-                .attr("x", d => xSubgroup(d.key))
-                .attr("y", d => y(d.value))
-                .attr("width", xSubgroup.bandwidth())
-                .attr("height", d => height - y(d.value))
-                .attr("fill", d => color(d.key))
-                .on("mouseover", function (event, d) {
-                    tooltip.transition()
-                        .duration(200)
-                        .style("opacity", .9);
-                    tooltip.html(`<strong>${d.key}</strong>: ${d.value}`)
-                        .style("left", (event.pageX + 10) + "px")
-                        .style("top", (event.pageY - 28) + "px");
-                    
-                })
-                .on("mouseout", function (d) {
-                    tooltip.transition()
-                        .duration(500)
-                        .style("opacity", 0);
-                    d3.select(this).attr('opacity', 1).attr('fill', d => color(d.key));
-                });
+    svg.append("g")
+        .selectAll("g")
+        .data(data)
+        .join("g")
+        .attr("transform", d => `translate(${x(d.date)}, 0)`)
+        .selectAll("rect")
+        .data(function (d) { return subgroups.map(function (key) { return { key: key, value: d[key] }; }); })
+        .join("rect")
+        .attr("class", d => d.key) // Assign class based on subgroup (city name)
+        .attr("x", d => xSubgroup(d.key))
+        .attr("y", d => y(d.value))
+        .attr("width", xSubgroup.bandwidth())
+        .attr("height", d => height - y(d.value))
+        .attr("fill", d => color(d.key))
+        .on("mouseover", function (event, d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html(`<strong>${d.key}</strong>: ${d.value}`)
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 28) + "px");
 
-            const legendLabels = ["San Francisco", "San Jose", "Oakland", "Fremont"];
-
-            const legend = svg.append("g")
-                .attr("transform", `translate(${width - 100},${margin.top})`);
-
-            legendLabels.forEach((label, i) => {
-                const legendRow = legend.append("g")
-                    .attr("transform", `translate(0, ${i * 20})`);
-
-                legendRow.append("rect")
-                    .attr("width", 10)
-                    .attr("height", 10)
-                    .attr("fill", color(subgroups[i]));
-
-                legendRow.append("text")
-                    .attr("x", 15)
-                    .attr("y", 10)
-                    .attr("dy", ".35em")
-                    .text(label);
-            });
-
-            function update() {
-                d3.selectAll(".checkbox").each(function () {
-                    const cb = d3.select(this);
-                    const grp = cb.property("value");
-                    console.log("Checkbox:", grp, "Checked:", cb.property("checked"));
-
-                    svg.selectAll(`.${grp}`)
-                        .transition()
-                        .duration(1000)
-                        .style("opacity", cb.property("checked") ? 1 : 0);
-                });
-            }
-
-            d3.selectAll(".checkbox").on("change", update);
-
-            update()
         })
+        .on("mouseout", function (d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+            d3.select(this).attr('opacity', 1).attr('fill', d => color(d.key));
+        });
+
+    const legendLabels = ["San Francisco", "San Jose", "Oakland", "Fremont"];
+
+    const legend = svg.append("g")
+        .attr("transform", `translate(${width - 100},${margin.top})`);
+
+    legendLabels.forEach((label, i) => {
+        const legendRow = legend.append("g")
+            .attr("transform", `translate(0, ${i * 20})`);
+
+        legendRow.append("rect")
+            .attr("width", 10)
+            .attr("height", 10)
+            .attr("fill", color(subgroups[i]));
+
+        legendRow.append("text")
+            .attr("x", 15)
+            .attr("y", 10)
+            .attr("dy", ".35em")
+            .text(label);
+    });
+
+    function update() {
+        d3.selectAll(".container input[type='checkbox']").each(function () {
+            const cb = d3.select(this);
+            const grp = cb.property("value");
+            console.log("Checkbox:", grp, "Checked:", cb.property("checked"));
+
+            svg.selectAll(`.${grp}`)
+                .transition()
+                .duration(1000)
+                .style("opacity", cb.property("checked") ? 1 : 0);
+        });
+    }
+
+
+    d3.selectAll(".container input[type='checkbox']").on("change", update);
+
+    update()
+})
